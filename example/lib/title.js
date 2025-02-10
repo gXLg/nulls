46c554919832134d2605a3cee645b659bff03092
gXLg <natgaev@gmail.com>
2025-02-10 04:09:57 +0100

    fix the example

--- example/lib/title.js
+++ example/lib/title.js
@@ -1,13 +1,3 @@
-module.exports = (path, auth) => {
-  if (path == "/") return "Null Chat";
-  if (path == "/chat") return "Null Chat - Chatting";
-  if (path == "/login") return "Null Chat - Login";
-  if (path == "/register") return "Null Chat - Register";
-  if (path.startsWith("/chat/")) {
-    if (!auth) return "Null Chat";
-    const [empty, chat, id, ...x] = path.split("/");
-    if (x.length || !id.match(/^[a-z0-9_]+$/i)) "Null Chat";
-    return "Null Chat - " + id;
-  }
-  return "Null Chat";
+module.exports = (req, res) => {
+  return "Nulls Example - " + req.path.toLowerCase();
 };
